c45d28c4c400dcf30db335a634b2df304860dd5c
Satotani <satotani@momo.cs.okayama-u.ac.jp>
2019-06-18 18:30:09 +0900

    Add model class to make model hold parameter

--- chimeranet/model.py
+++ chimeranet/model.py
@@ -5,51 +5,67 @@ from keras.models import Model
 from keras.layers import Input, Lambda, Reshape, Activation
 from keras.layers import Dense, LSTM, Bidirectional
 
-def loss_deepclustering(y_true, y_pred):
-    C, (_, T, F, D) = K.int_shape(y_true)[-1], K.int_shape(y_pred)
-    if C is None:
-        C = 1
-    y_true_flatten = K.reshape(y_true, (-1, T*F, C))
-    y_pred_flatten = K.reshape(y_pred, (-1, T*F, D))
-    A = K.batch_dot(
-        y_true_flatten,
-        K.permute_dimensions(y_true_flatten, (0, 2, 1)),
-        axes=(2, 1)
-    )
-    Ahat = K.batch_dot(
-        y_pred_flatten,
-        K.permute_dimensions(y_pred_flatten, (0, 2, 1)),
-        axes=(2, 1)
-    )
-    return K.sum(K.pow(A - Ahat, 2), axis=(1, 2))\
-        / K.cast(F*T, K.floatx())
+class ChimeraNetModel:
+    def __init__(self, time_frames, mel_bins, n_channels, d_embeddings):
+        self.T = time_frames
+        self.F = mel_bins
+        self.C = n_channels
+        self.D = d_embeddings
+    def loss_deepclustering(self):
+        def _loss_deepclustering(y_true, y_pred):
+            y_true_flatten = K.reshape(y_true, (-1, self.T*self.F, self.C))
+            y_pred_flatten = K.reshape(y_pred, (-1, self.T*self.F, self.D))
+            A = K.batch_dot(
+                y_true_flatten,
+                K.permute_dimensions(y_true_flatten, (0, 2, 1)),
+                axes=(2, 1)
+            )
+            Ahat = K.batch_dot(
+                y_pred_flatten,
+                K.permute_dimensions(y_pred_flatten, (0, 2, 1)),
+                axes=(2, 1)
+            )
+            return K.sum(K.pow(A - Ahat, 2), axis=(1, 2))\
+                / K.cast_to_floatx(self.F*self.T)
+        return _loss_deepclustering
 
-def loss_mask(y_true, y_pred):
-    _, T, F, C = K.int_shape(y_pred)
-    mixture = K.expand_dims(y_true[:, :, :, C])
-    return K.sum(
-        K.pow((y_true[:, :, :, :C] - y_pred)*mixture, 2),
-        axis=(1, 2, 3)
-    )
+    def loss_mask(self):
+        def _loss_mask(y_true, y_pred):
+            mixture = K.expand_dims(y_true[:, :, :, self.C])
+            return K.sum(
+                K.pow((y_true[:, :, :, :self.C] - y_pred)*mixture, 2),
+                axis=(1, 2, 3)
+            )
+        return _loss_mask
 
-def build_model(T, F, C, D, n_blstm_units=500, n_blstm_layers=4):
-    inputs = Input(shape=(T, F), name='input')
-    blstm_top = inputs
-    for i in range(1, n_blstm_layers+1):
-        blstm_top = Bidirectional(
-            LSTM(n_blstm_units, return_sequences=True),
-            name='body_blstm_{}'.format(i)
-        )(blstm_top)
-    body = Reshape((T, F, D), name='body_reshape')(Dense(F*D, name='body_linear')(blstm_top))
+    def build_model(self, n_blstm_units=500, n_blstm_layers=4):
+        inputs = Input(shape=(self.T, self.F), name='input')
+        blstm_top = inputs
+        for i in range(1, n_blstm_layers+1):
+            blstm_top = Bidirectional(
+                LSTM(n_blstm_units, return_sequences=True),
+                name='body_blstm_{}'.format(i)
+            )(blstm_top)
+        body_linear = Dense(self.F*self.D, name='body_linear')(blstm_top)
+        body = Reshape((self.T, self.F, self.D), name='body')(body_linear)
 
-    embedding = Lambda(lambda x: K.l2_normalize(x, axis=-1), name='embedding')(
-        Activation('tanh', name='embedding_activation')(body)
-    )
-    mask = Lambda(lambda x: K.stack(x, axis=2), name='mask')([
-        Dense(C, activation='softmax', name='mask_linear_{}'.format(f+1))(
-            Lambda(lambda x: x[:, :, f, :], name='mask_slice_{}'.format(f+1))(body)
-        ) for f in range(F)
-    ])
+        embd_activation = Activation('tanh', name='embedding_activation')(body)
+        embedding = Lambda(
+            lambda x: K.l2_normalize(x, axis=-1), name='embedding'
+        )(embd_activation)
+        mask_slices = [
+            Lambda(
+                lambda x: x[:, :, f, :], name='mask_slice_{}'.format(f+1)
+            )(body)
+            for f in range(self.F)
+        ]
+        mask_linears = [
+            Dense(
+                self.C, activation='softmax', name='mask_linear_{}'.format(f+1)
+            )(mask_slices[f])
+            for f in range(self.F)
+        ]
+        mask = Lambda(lambda x: K.stack(x, axis=2), name='mask')(mask_linears)
     
-    model = Model(inputs=inputs, outputs=[embedding, mask])
-    return model
+        model = Model(inputs=inputs, outputs=[embedding, mask])
+        return model
--- scripts/chimeranet-train-dsd100.py
+++ scripts/chimeranet-train-dsd100.py
@@ -1,41 +1,49 @@
 #!/usr/env/bin python
 import os, sys
 import numpy as np
+import pickle
 import h5py
 import librosa
 
 sys.path.append(os.path.join(os.path.split(__file__)[0], '..'))
-import chimeranet.model
+from chimeranet.model import ChimeraNetModel
 from chimeranet.audio_sampler import SpectrogramSampler
 from chimeranet.dataset_loader.dsd100\
     import DSD100MelodyLoader, DSD100VocalLoader
 from chimeranet.preprocessing import to_mixture, to_true_pair
 
 def main():
-    # build spectrogram sampler
+    # parameters
     time = 0.5
     sr, n_fft, hop_length, n_mels = 16000, 512, 128, 64
     dataset_path = 'dataset_dsd100.h5'
     model_path = 'model_dsd100.h5'
+    history_path = 'history_dsd100.json.pkl'
+
+    # build spec. sampler for training and validation
     ss = SpectrogramSampler()
     ss.add_reader(DSD100VocalLoader('DSD100.zip'))
     ss.add_reader(DSD100MelodyLoader('DSD100.zip'))
     ss.time(time).n_mels(n_mels).sr(sr).n_fft(n_fft).hop_length(hop_length)
-    ss.augment_amp_range(-5, 5)
-    ss.augment_time_range(0.7, 1.3)
-    ss.sync_flag(False)
+    ss.augment_time_range(2/3, 4/3).augment_freq_range(-0.5, 0.5).augment_amp_range(-10, 10)
+    ss.sync_flag(True)
+
+    ss_val = SpectrogramSampler()
+    ss_val.add_reader(DSD100VocalLoader('DSD100.zip', validation=True))
+    ss_val.add_reader(DSD100MelodyLoader('DSD100.zip', validation=True))    
+    ss_val.time(time).n_mels(n_mels).sr(sr).n_fft(n_fft).hop_length(hop_length)
+    ss_val.sync_flag(True)
     
     # build model
-    T = ss.get_frames()
-    F = n_mels
-    C = ss.get_number_of_channels()
-    D = 20
-    model = chimeranet.model.build_model(T, F, C, D)
+    cm = ChimeraNetModel(
+        ss.get_frames(), n_mels, ss.get_number_of_channels(), 20
+    )
+    model = cm.build_model()
     model.compile(
         'rmsprop',
         loss={
-            'embedding': chimeranet.model.loss_deepclustering,
-            'mask': chimeranet.model.loss_mask
+            'embedding': cm.loss_deepclustering(),
+            'mask': cm.loss_mask()
         },
         loss_weights={
             'embedding': 0.5,
@@ -44,14 +52,26 @@ def main():
     )
 
     # obtain training data from spectrogram sampler and train model
-    sample_size, batch_size = 256, 1
-    specs = ss.make_specs(sample_size)
+    sample_size, batch_size = 16000, 32
+    specs_train = ss.make_specs(sample_size, 50)
+    specs_valid = ss_val.make_specs(sample_size // 100, 50)
     with h5py.File(dataset_path, 'w') as f:
-        f.create_dataset('specs', data=specs)
-    x = to_mixture(specs)
-    y = to_true_pair(specs)
-    model.fit(x=x, y=y, batch_size=batch_size, epochs=200)
+        f.create_dataset('specs_train', data=specs_train)
+        f.create_dataset('specs_valid', data=specs_valid)
+    x_train = to_mixture(specs_train)
+    y_train = to_true_pair(specs_train)
+    x_valid = to_mixture(specs_valid)
+    y_valid = to_true_pair(specs_valid)
+    history = model.fit(
+        x=x_train,
+        y=y_train,
+        batch_size=batch_size,
+        epochs=200,
+        validation_data=(x_valid, y_valid)
+    )
     model.save(model_path)
+    with open(history_path, 'wb') as fp:
+        pickle.dump(history.history, fp)
 
 if __name__ == '__main__':
     main()
