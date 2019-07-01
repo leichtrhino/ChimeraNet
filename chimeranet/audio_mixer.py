1f188b54a436b7222ce06a6e2265fb40e0b3b845
arity-r <yoshikimethod@hotmail.co.jp>
2019-07-01 12:38:13 +0900

    Rename audio_sampler -> audio_mixer and separate time/freq/amp sync/async mode

--- chimeranet/audio_mixer.py
+++ chimeranet/audio_mixer.py
@@ -7,30 +7,45 @@ import scipy
 
 from .audio_loader import AudioLoader
 
-class SpectrogramSampler:
+class AudioMixer:
     def __init__(self):
         self._audio_readers = []
-        self.sync_flag(True)
+        self._augment_time_list = []
+        self._augment_freq_list = []
+        self._augment_amp_list = []
         self.time(.5)
         self.n_mels(64)
         self.sr(16000)
         self.n_fft(2048)
         self.hop_length(512)
-        self.augment_time_range(1, 1)
-        self.augment_freq_range(0, 0)
-        self.augment_amp_range(0, 0)
+        self.sync_flag(True)
 
-    def add_reader(self, audio_reader):
-        self._audio_readers.append(audio_reader)
+    def add_loader(
+        self, audio_loader, a_time=(1, 1), a_freq=(0, 0), a_amp=(0, 0)):
+        self._audio_readers.append(audio_loader)
+        self._augment_time_list.append(a_time)
+        self._augment_freq_list.append(a_freq)
+        self._augment_amp_list.append(a_amp)
+        return self
+
+    def sync_flag(self, flag):
+        self.sync_time_flag(flag)
+        self.sync_freq_flag(flag)
+        self.sync_amp_flag(flag)
         return self
     
-    def add_loader(self, audio_loader):
-        return self.add_reader(audio_loader)
-
-    def sync_flag(self, sync_flag):
-        self._sync_flag = sync_flag
+    def sync_time_flag(self, flag):
+        self._sync_time_flag = flag
         return self
-    
+
+    def sync_freq_flag(self, flag):
+        self._sync_freq_flag = flag
+        return self
+
+    def sync_amp_flag(self, flag):
+        self._sync_amp_flag = flag
+        return self
+
     def time(self, t):
         self._time_in_sec = t
         return self
@@ -51,18 +66,6 @@ class SpectrogramSampler:
         self._hop_length = h
         return self
 
-    def augment_time_range(self, lo, hi):
-        self._augment_time_lo, self._augment_time_hi = lo, hi
-        return self
-
-    def augment_freq_range(self, lo, hi):
-        self._augment_freq_lo, self._augment_freq_hi = lo, hi
-        return self
-
-    def augment_amp_range(self, lo, hi):
-        self._augment_amp_lo, self._augment_amp_hi = lo, hi
-        return self
-    
     def get_number_of_channels(self):
         return len(self._audio_readers)
     
@@ -86,15 +89,15 @@ class SpectrogramSampler:
             np.random.shuffle(idx)
             return idx[:N]
         
-        if self._sync_flag and min_len > n_loads * 100:
+        if self._sync_time_flag and min_len > n_loads * 100:
             # large audio library, make sample_size random indices
             return make_random(n_loads, min_len)\
                 .repeat(n_channels).reshape((n_loads, n_channels)).T
-        elif self._sync_flag and min_len <= n_loads * 100:
+        elif self._sync_time_flag and min_len <= n_loads * 100:
             # small audio library, make 0-sample_size array and shuffle
             return make_range_shuffle(n_loads, min_len)\
                 .repeat(n_channels).reshape((n_loads, n_channels)).T
-        else: # if not self._sync_flag
+        else: # if not self._sync_time_flag
             return [
                 make_random(lpc, len(ar))
                 if len(ar) > lpc * 100 else
@@ -102,41 +105,71 @@ class SpectrogramSampler:
                 for lpc, ar in zip(loads_per_channel, self._audio_readers)
             ]
         # end function
+
+    def _mod_time(self, audio_list):
+        if self._sync_time_flag:
+            min_time_rate = max(t[0] for t in self._augment_time_list)
+            max_time_rate = min(t[1] for t in self._augment_time_list)
+            assert min_time_rate <= max_time_rate,\
+                'Invalid time augmentation'
+            rates = [random.uniform(min_time_rate, max_time_rate)]\
+                * len(audio_list)
+            offsets = [random.uniform(0, 1)] * len(audio_list)
+        else:
+            rates = [
+                random.uniform(*t) for t in self._augment_time_list
+            ]
+            offsets = [random.uniform(0, 1) for _ in self._augment_time_list]
+        def mod_single(t):
+            a, r, o = t
+            n_slice_samples = librosa.core.time_to_samples(
+                self._time_in_sec / r, sr=self._sr
+            )
+            offset = max(0, int(o * (a.size - n_slice_samples)))
+            # slice
+            sliced = a if a.size < offset + n_slice_samples\
+                else a[offset:offset+n_slice_samples]
+            # stretch
+            stretched = librosa.effects.time_stretch(sliced, r)
+            return stretched
+        return list(map(mod_single, zip(audio_list, rates, offsets)))
+
+    def _mod_freq(self, audio_list):
+        if self._sync_freq_flag:
+            min_freq_rate = max(t[0] for t in self._augment_freq_list)
+            max_freq_rate = min(t[1] for t in self._augment_freq_list)
+            assert min_freq_rate <= max_freq_rate,\
+                'Invalid freq augmentation'
+            rates = [random.uniform(min_freq_rate, max_freq_rate)]\
+                * len(audio_list)
+        else:
+            rates = [random.uniform(*r) for r in self._augment_freq_list]
+        bins_per_octave = self._n_mels * 8
+        def mod_single(t):
+            a, r = t
+            n_step = int(r * bins_per_octave)
+            return librosa.effects.pitch_shift(
+                a, self._sr, n_step, bins_per_octave
+            )
+        return list(map(mod_single, zip(audio_list, rates)))
+    
+    def _mod_amp(self, audio_list):
+        if self._sync_amp_flag:
+            min_amp_rate = max(t[0] for t in self._augment_amp_list)
+            max_amp_rate = min(t[1] for t in self._augment_amp_list)
+            assert min_amp_rate <= max_amp_rate,\
+                'Invalid freq augmentation'
+            rates = [random.uniform(min_amp_rate, max_amp_rate)]\
+                * len(audio_list)
+        else:
+            rates = [random.uniform(*r) for r in self._augment_amp_list]
+        def mod_single(t):
+            a, r = t
+            return a * 10 ** (r / 10)
+        return list(map(mod_single, zip(audio_list, rates)))
     
     def _transform_specs(self, raw_spec_list):
-        # transform time, pitch, amplitude
-        time_lo, time_hi = self._augment_time_lo, self._augment_time_hi
-        if self._sync_flag:
-            time_rates = [random.uniform(time_lo, time_hi)] * len(raw_spec_list)
-        else:
-            time_rates = [
-                random.uniform(time_lo, time_hi) for _ in raw_spec_list
-            ]
-        freq_lo, freq_hi = self._augment_freq_lo, self._augment_freq_hi
-        orig_height = min(spec.shape[0] for spec in raw_spec_list)
-        if self._sync_flag:
-            freq_rates = [
-                2 ** random.uniform(freq_lo, freq_hi)
-            ] * len(raw_spec_list)
-        else:
-            freq_rates = [
-                2 ** random.uniform(freq_lo, freq_hi) for _ in raw_spec_list
-            ]
-        mod_specs = [
-            scipy.ndimage.zoom(s, (fr, 1/tr), order=0)
-            for s, tr, fr in zip(raw_spec_list, time_rates, freq_rates)
-        ]
-        mod_specs = [
-            np.vstack((s, np.zeros((orig_height - s.shape[0], s.shape[1]))))
-            if s.shape[0] < orig_height else s[:orig_height, :]
-            for s in mod_specs
-        ]
-        amp_lo, amp_hi = self._augment_amp_lo, self._augment_amp_hi
-        mod_specs = [
-            s * 10 ** (random.uniform(amp_lo, amp_hi) / 10)
-            for s in mod_specs
-        ]
-
+        mod_specs = raw_spec_list
         T = librosa.core.time_to_frames(
             self._time_in_sec, sr=self._sr, hop_length=self._hop_length
         )
@@ -170,7 +203,7 @@ class SpectrogramSampler:
             ]
         if len(loads_per_channel) != len(self._audio_readers):
             raise ValueError('length of loads_per_channel does not match')
-        if self._sync_flag\
+        if self._sync_time_flag\
             and any(l != loads_per_channel[0] for l in loads_per_channel):
             raise ValueError('different loads for time sync mode')
         # n_channel x n_loads
@@ -179,6 +212,9 @@ class SpectrogramSampler:
             [a.load_audio(i, self._sr) for mi, i in enumerate(ii) if mi < loads_per_channel[li]]
             for li, (a, ii) in enumerate(zip(self._audio_readers, idx))
         ]
+        mod = lambda a: self._mod_amp(self._mod_freq(self._mod_time(a)))
+        raw_audio_list = list(zip(*map(mod, zip(*raw_audio_list))))
+
         raw_spec_list = [
             [
                 np.abs(librosa.core.stft(audio, self._n_fft, self._hop_length))
@@ -187,7 +223,7 @@ class SpectrogramSampler:
             for channel in raw_audio_list
         ]
         # n_sample x n_channel
-        if self._sync_flag:
+        if self._sync_time_flag:
             spec_idx = list(range(max(loads_per_channel)))\
                 *int(math.ceil(sample_size / max(loads_per_channel)))
             random.shuffle(spec_idx)
