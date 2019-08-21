aac366fe811e6b18e2abdcce9bf2f566ffd1d710
arity-r <yoshikimethod@hotmail.co.jp>
2019-08-21 10:31:55 +0900

    Add threshold level to mask maker

--- chimeranet/preprocessing.py
+++ chimeranet/preprocessing.py
@@ -15,14 +15,14 @@ input: NxCxFxT tensor
 output: 'embedding': NxTxFxC tensor
         'mask': NxTxFx(C+1) tensor
 """
-def to_true_pair(multichannels, mask='linear'):
+def to_true_pair(multichannels, mask='linear', threshold=None):
     mixture = np.expand_dims(to_mixture(multichannels), axis=-1)
     mask_func = _binary_mask if mask == 'binary' else\
         _softmax_mask if mask == 'softmax' else _linear_mask
     return {
-        'embedding': _binary_mask(multichannels).transpose((0, 3, 2, 1)),
+        'embedding': _binary_mask(multichannels, threshold).transpose((0, 3, 2, 1)),
         'mask': np.concatenate(
-            (mask_func(multichannels).transpose((0, 3, 2, 1)), mixture),
+            (mask_func(multichannels, threshold).transpose((0, 3, 2, 1)), mixture),
             axis=-1
         )
     }
@@ -31,28 +31,35 @@ def to_true_pair(multichannels, mask='linear'):
 input: NxCxFxT tensor
 output: NxCxFxT tensor
 """
-def _binary_mask(multichannels):
+def _binary_mask(multichannels, threshold=None):
     m = multichannels.transpose((0, 3, 2, 1)) # => NxTxFxC
     C = m.shape[-1]
     masks = np.eye(C)[
         m.reshape((m.size // C, C)).argmax(axis=-1)
-    ].reshape(m.shape) # => NxTxFxC
-    return masks.transpose((0, 3, 2, 1)) # => NxCxFxT
+    ].reshape(m.shape).transpose((0, 3, 2, 1)) # => NxCxFxT
+    if threshold is not None and threshold > 0:
+        masks = np.where(multichannels > threshold, masks, 0)
+    return masks
 
 """
 input: NxCxFxT tensor
 output: NxCxFxT tensor
 """
-def _linear_mask(multichannels):
-    m = multichannels.transpose((0, 3, 2, 1)) # => NxTxFxC
-    C = m.shape[-1]
-    mixture = np.expand_dims(m.sum(axis=-1), -1) # => NxTxFx1
+def _linear_mask(multichannels, threshold=None):
+    m = multichannels
+    C = m.shape[1]
+    mixture = np.expand_dims(m.sum(axis=1), 1)
     masks = np.clip(m / np.maximum(mixture, 1e-32), 0, 1)
-    return masks.transpose((0, 3, 2, 1)) # => NxCxFxT
+    if threshold is not None and threshold > 0:
+        masks = np.where(multichannels > threshold, masks, 0)
+    return masks
 
 """
 input: NxCxFxT tensor
 output: NxCxFxT tensor
 """
-def _softmax_mask(multichannels):
-    return softmax(multichannels, axis=1)
+def _softmax_mask(multichannels, threshold=None):
+    masks = softmax(multichannels, axis=1)
+    if threshold is not None and threshold > 0:
+        masks = np.where(multichannels > threshold, masks, 0)
+    return masks
